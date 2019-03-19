e047d8b1fe0b06968c422c32a941f91efaa28203
Gustavo Nobrega <gustavonobrega@gmail.com>
2019-03-18 22:41:53 -0300

    Supress unnecessary races

--- app/src/main/java/com/timsoft/meurebanho/specie/model/Specie.java
+++ app/src/main/java/com/timsoft/meurebanho/specie/model/Specie.java
@@ -14,11 +14,11 @@ public class Specie extends IDDescription {
 
     public static List<Specie> getDefaultSpecies() {
         List<Specie> e = new ArrayList<Specie>();
-        e.add(new Specie(1, MeuRebanhoApp.getContext().getResources().getString(R.string.specie_bovine)));
-        e.add(new Specie(2, MeuRebanhoApp.getContext().getResources().getString(R.string.specie_caprine)));
-        e.add(new Specie(3, MeuRebanhoApp.getContext().getResources().getString(R.string.specie_equine)));
-        e.add(new Specie(4, MeuRebanhoApp.getContext().getResources().getString(R.string.specie_ovine)));
-        e.add(new Specie(5, MeuRebanhoApp.getContext().getResources().getString(R.string.specie_swine)));
+        //e.add(new Specie(1, MeuRebanhoApp.getContext().getResources().getString(R.string.specie_bovine)));
+        e.add(new Specie(1, MeuRebanhoApp.getContext().getResources().getString(R.string.specie_caprine)));
+        //e.add(new Specie(3, MeuRebanhoApp.getContext().getResources().getString(R.string.specie_equine)));
+        e.add(new Specie(2, MeuRebanhoApp.getContext().getResources().getString(R.string.specie_ovine)));
+        //e.add(new Specie(5, MeuRebanhoApp.getContext().getResources().getString(R.string.specie_swine)));
         return e;
     }
 }
